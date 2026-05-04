29412566067359e8a90c4e70ff7bd6a203cd68cb
ez-plugins <ezplugins@outlook.com>
2026-05-04 22:09:41 +0200

    chore: add AGPL-3.0 copyright headers and NOTICE.md
    Add copyright headers to all 87 Java source files:
    - Plugin module: dual copyright (MCBans authors + PvPIndex 2026)
    - API client module: PvPIndex-only copyright (new code)
    
    Add NOTICE.md with full AGPL-3.0 attribution:
    - Original MCBans authors and source URL
    - List of substantial modifications made by this fork
    - Explicit AGPL-3.0 (no 'or later') statement per legal advice
    
    Update README: replace sparse 'Credits' + 'License' sections with a
    proper 'License and attribution' section listing original authors,
    original source, and modifications.

--- pvpindex-mcbans-plugin/src/main/java/com/pvpindex/bans/plugin/request/Kick.java
+++ pvpindex-mcbans-plugin/src/main/java/com/pvpindex/bans/plugin/request/Kick.java
@@ -1,3 +1,12 @@
+/*
+ * This file is part of PvPIndex MCBans, a modified fork of MCBans.
+ *
+ * Original work Copyright (C) MCBans authors and contributors.
+ * Modifications Copyright (C) 2026 PvPIndex contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License version 3.
+ */
 package com.pvpindex.bans.plugin.request;
 
 import static com.pvpindex.bans.plugin.I18n.localize;
